826b58b2d70c52498023a70aa48f2a5a0cef69e9
mgzgithub <1759117095@qq.com>
2021-05-05 15:40:46 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        //默认有@  他指向的是：src
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
+}
